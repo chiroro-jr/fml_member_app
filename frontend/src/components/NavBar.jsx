9997fa1022d72c08add75972579984b3f5ff2f79
Nyasha D. Chiroro <chirorodennis@gmail.com>
2022-08-19 13:55:58 +0200

    Add new navbar styles

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,19 +1,24 @@
-import { NavLink, useNavigate } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
+import Logo from './Logo'
 
 function NavBar() {
-    const navigate = useNavigate()
-
     return (
         <nav className="nav">
-            <div className="nav__container">
-                <h1 className="nav__logo">Member Application</h1>
-                <div className="nav__links">
-                    <NavLink to="/" className="nav__link">
-                        Home
-                    </NavLink>
-                    <NavLink to="/new" className="nav__link">
-                        New Member
-                    </NavLink>
+            <div className="nav__blur-outer">
+                <div className="nav__blur-inner">
+                    <div className="nav__container">
+                        <Link to="/">
+                            <Logo />
+                        </Link>
+                        <div className="nav__links">
+                            <NavLink to="/" className="nav__link">
+                                Home
+                            </NavLink>
+                            <NavLink to="/new" className="nav__link">
+                                Add Member
+                            </NavLink>
+                        </div>
+                    </div>
                 </div>
             </div>
         </nav>
