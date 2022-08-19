f33db2c03ca1a869e64b54f857e39f9bcc35b7c0
Nyasha D. Chiroro <chirorodennis@gmail.com>
2022-08-19 13:56:14 +0200

    Add fmhl logo

---
+++ frontend/src/components/Logo.jsx
@@ -0,0 +1,13 @@
+function Logo() {
+    return (
+        <div className="logo">
+            <img
+                src="/fmhl-logo.svg"
+                alt="First Mutual Logo"
+                className="logo__img"
+            />
+        </div>
+    )
+}
+
+export default Logo
